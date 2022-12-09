6bdb1403e423dbd5cf3a4226472ce51972494b0a
Ft0om <fatima.khalil016@gmail.com>
2022-12-09 13:10:10 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - main block
@@ -7,11 +6,8 @@
  *   * Return: 0
  *
  *    */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
