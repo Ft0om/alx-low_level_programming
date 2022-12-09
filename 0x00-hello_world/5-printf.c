5cfc22789f98a9fea894038f5def30b3c58199a7
Ft0om <fatima.khalil016@gmail.com>
2022-12-09 13:50:56 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
-*main : main block
-*return: 0
+* main - main block
+* Return: 0
 */
 int main(void)
+
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+
+	return (0);
+
 }
