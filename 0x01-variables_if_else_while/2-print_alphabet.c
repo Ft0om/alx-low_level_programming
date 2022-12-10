eef1600d99c1cfab335f0f3a92ed90ccaab83ac8
Ft0om <fatima.khalil016@gmail.com>
2022-12-10 11:21:19 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,12 @@ int main(void)
 
 	char c = 'a';
 
-        while (c <= 'z' )
+	while (c <= 'z')
 	{
-		putchar(c);
+		putchar( c );
 		c++;
 	}
 
 	putchar('\n');
-        return (0);
+	return (0);
 }
