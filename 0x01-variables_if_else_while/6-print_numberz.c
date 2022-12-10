bd34ff6b1a45bc25a30ff74e83c548b7377debe7
Ft0om <fatima.khalil016@gmail.com>
2022-12-10 13:00:37 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - write numbers
+ *
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i%10 + 'i');
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
