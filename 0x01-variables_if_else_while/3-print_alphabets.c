eef116586bc619613ff61b4f0af8f4d4676492da
Ft0om <fatima.khalil016@gmail.com>
2022-12-10 11:30:04 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print alphabets in lower then in upper
+ * Return: 0
+ */
+int main(void)
+{
+	char ce = 'a';
+
+	while (ce <= 'z')
+	{
+		putchar(ce);
+		ce++;
+	}
+	
+	ce = 'A';
+
+	while (ce <= 'Z')
+	{
+		putchar(ce);
+		ce++;
+	}
+
+	putchar('\n')
+	return (0);
+}
