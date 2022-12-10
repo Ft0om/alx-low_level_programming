96a4e81dd4f1212277fa58076f742f7aff9660d8
Ft0om <fatima.khalil016@gmail.com>
2022-12-10 11:45:05 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Discription: alphabets expect q and e
+ * Retun: 0
+ */
+int main(void)
+{
+	char ce = 'a':
+	
+	while (ce <= 'a')
+	{
+		if (ce != 'q' || 'e')
+		putchar(ce);
+		ce++;
+	}
+	putchar('\n');
+	return (0);
+}
