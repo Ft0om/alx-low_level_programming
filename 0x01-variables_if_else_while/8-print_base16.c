cae942212751b69a3f462010a4e851041ce4c14b
Ft0om <fatima.khalil016@gmail.com>
2022-12-10 13:28:31 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	int c;
+	char i;
+
+	for (c = 0; c<=9; c++)
+		putchar(c % 10 + '0');
+
+	for (i = 'a';i <= 'f';i++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
