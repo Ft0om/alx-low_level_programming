d860886f66accbe9983b19623efe88ed82079673
Ft0om <fatima.khalil016@gmail.com>
2022-12-11 23:45:33 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		putchar(,);
+		putchar( );
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
