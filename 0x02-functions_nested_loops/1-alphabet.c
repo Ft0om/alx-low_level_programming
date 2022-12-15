e94b1e500a7a01f83ac0680d1de67700c01aa810
Ft0om <fatima.khalil016@gmail.com>
2022-12-15 02:28:05 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#inclue "main.h"
+/**
+ * main - main block
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
