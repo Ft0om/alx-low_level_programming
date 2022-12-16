b0a77e698db222b958c224024520dde245aabe77
Ft0om <fatima.khalil016@gmail.com>
2022-12-16 10:47:49 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check case of char.
+ * @c: input to be checked
+ * Return: 1 if c is upper, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') $$ (c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
