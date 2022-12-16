10cb9d7f53c00694b2f0b5be087661fc9e96c4ac
Ft0om <fatima.khalil016@gmail.com>
2022-12-16 10:52:56 -0800

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +15,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+
+#endif 
