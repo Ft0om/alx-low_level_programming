5f82b4e59c7314c178eb6aac8455e0b7a983484b
Ft0om <fatima.khalil016@gmail.com>
2022-12-16 11:36:39 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,16 @@
 
 void more_numbers(void)
 {
-	char i, j;
+	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
-			_putchar(j);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
