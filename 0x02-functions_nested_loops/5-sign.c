e8cf1bd60e3492bdb047dbc38f53453d4a428994
Ft0om <fatima.khalil016@gmail.com>
2022-12-17 22:37:31 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,22 @@
  */
 int print_sign(int n)
 {
+	int i;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		i = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		i = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		i = -1;
 	}
+	return (i);
 }
