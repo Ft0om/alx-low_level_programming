6b042bd3c5423c55641b0fbd28c74946e57afe96
Ft0om <fatima.khalil016@gmail.com>
2022-12-19 06:23:53 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,4 +6,15 @@
  */
 void print_rev(char *s)
 {
+	int i, n;
 
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string
+ * @str: input
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+
